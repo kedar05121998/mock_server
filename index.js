c63da7a01c1e4c1b68322b5e6d1557d1b364597e
kedar05121998 <101327577+kedar05121998@users.noreply.github.com>
2023-01-10 12:07:41 +0530

    dogs data

--- index.js
+++ index.js
@@ -1,10 +1,39 @@
-const jsonServer = require("json-server"); // importing json-server library
-const server = jsonServer.create();
-const router = jsonServer.router("db.json");
-const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3001; // you can use any port number here; i chose to use 3001
+const jsonServer = require('json-server')
+const auth = require('json-server-auth')
+const server = jsonServer.create()
+const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults()
+server.db = router.db
+// server.bind(router)
 
-server.use(middlewares);
-server.use(router);
+// const rules = auth.rewriter({
+//   // Permission rules
+//   users: 600,
+//   messages: 640,
+//   // Other rules
+//   '/posts/:category': '/posts?category=:category',
+// })
 
-server.listen(port);
+// You must apply the middlewares in the following order
+
+
+server.use(middlewares)
+server.use(auth)
+// server.use(rules)
+// Add this before server.use(router)
+server.use(jsonServer.rewriter({
+    '/api/*': '/$1',
+    '/blog/:resource/:id/show': '/:resource/:id'
+}))
+server.use(jsonServer.bodyParser)
+server.use(router)
+server.listen(3000, () => {
+  console.log('JSON Server is running')
+})
+
+
+// Export the Server API
+module.exports = server
+
+// json-server-auth backend
+
